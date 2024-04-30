a960e6ef44ea69fe735e9b8d7378d76b530cfd0c
Litzy Yulissa Nevarez Garcia <19040372@itdurango.edu.mx>
2024-04-30 14:51:14 -0600

    primer commit

---
+++ src/componentes/Tarea.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../css/Tarea.css';
+
+function Tarea({texto}){
+    return(
+        <div className="tarea-contenedor">
+            <div className="tarea-texto">{texto}</div>
+            <div className="tarea-icono">
+                Eliminar
+            </div>
+        </div>
+    );
+}
+
+export default Tarea;
